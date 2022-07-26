d7720ca13565c15c5e88717ae9c844a28ad7e428
unknown <kris.topic@gmail.com>
2022-07-27 00:15:20 +0200

    Add CityCard component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,14 @@
 import Navigation from "./Navigation";
 import Header from "./Header";
 import Footer from "./Footer";
+import CityCard from "./CityCard";
 
 const Main = () => {
   return (
     <div>
         <Navigation/>
         <Header/>
+        <CityCard/>
         <Footer/>
     </div>
   )
