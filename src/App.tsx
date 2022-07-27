9caa43efcce3ca722ea744bd9015fa236bbd1eb4
unknown <kris.topic@gmail.com>
2022-07-27 18:22:49 +0200

    Add Accommodation Details component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import Main from "./components/Main";
+import { Routes, Route } from "react-router-dom";
+import AccommodationDetails from "./components/AccommodationDetails";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Main/>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/accommodation-details"
+          element={<AccommodationDetails />}
+        />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,4 @@
-import Navigation from "./Navigation";
 import Header from "./Header";
-import Footer from "./Footer";
 import CityCard from "./CityCard";
 import AccommodationCard from "./AccommodationCard";
 import PlaceCard from "./PlaceCard";
@@ -8,12 +6,10 @@ import PlaceCard from "./PlaceCard";
 const Main = () => {
   return (
     <div>
-        <Navigation/>
         <Header/>
         <CityCard/>
         <AccommodationCard/>
         <PlaceCard/>
-        <Footer/>
     </div>
   )
 }
