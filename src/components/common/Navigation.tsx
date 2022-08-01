544c8500ac2bfaba1760fc7a4ae19572c1c80b65
unknown <kris.topic@gmail.com>
2022-08-01 13:47:09 +0200

    Update navigation component

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,16 +1,150 @@
-import "./style/navigation.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Locations", "My Places", "My Bookings"];
 
 const Navigation = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <div className="navigation">
-      <p className="logo">Staycation</p>
-      <ul className="navigation-links">
-        <li>Locations</li>
-        <li>My Places</li>
-        <li>My Bookings</li>
-      </ul>
-      <p className="logout">LOGOUT</p>
-    </div>
+    <AppBar position="static" sx={{ background: "#fff" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "Roboto",
+              fontWeight: 500,
+              letterSpacing: ".15px",
+              color: "#000",
+              textDecoration: "none",
+            }}
+          >
+            Staycation
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, color: "#1A3440" }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: " Roboto",
+              fontWeight: 500,
+              letterSpacing: ".15px",
+              color: "#000",
+              textDecoration: "none",
+              fontSize: "20px"
+            }}
+          >
+            Staycation
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", justifyContent: "center" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "#1A3440",
+                  display: "block",
+                  fontFamily: "Roboto",
+                  fontSize: "16px",
+                  textTransform: "none",
+                  textDecoration: "underline",
+                  letterSpacing: ".15px",
+                  fontWeight: 400,
+                  "&:hover": {
+                    textDecoration: "underline"
+                  }
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Button
+              sx={{
+                my: 2,
+                color: "#1A3440",
+                display: "block",
+                fontFamily: "Roboto",
+                fontSize: "14px",
+                fontWeight: 500,
+                letterSpacing: ".4px"
+              }}
+            >
+              LOGOUT
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
